c2e19156c3f66cbebdacddefc99709812544eeb9
Forward <Noah.Forward@k202domain.com>
2023-11-29 10:57:41 -0330

    Added documentation to the methods

--- tile.js
+++ tile.js
@@ -1,45 +1,85 @@
 const $ = selector => document.querySelector(selector);
 
+/*
+* Class to represent a tile with coordinates (x,y) and a value in the grid for the game
+ * */ 
 class Tile {
     value;
     x;
     y;
     pageElement;
+    
+    /*
+    * Builds a tilte object and sets its value and coordinates
+    * @param {value} value held by the tile as an integer
+    * @param  {x} X coordinate of the tile's location
+    * @param  {y} Y coordinate of the tile's location 
+    */
     constructor(value, x, y) {
         this.value = value;
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Get the value of the tile.
+     * @returns {number} The current value of the tile.
+     */
     getValue() {
         return this.value;
     }
 
+    /**
+     * Set the value of the tile.
+     * @param {number} newValue - The new value to set for the tile.
+     */
     setValue(newValue) {
         this.value = newValue;
     }
 
+     /**
+     * Get the x-coordinate of the tile.
+     * @returns {number} The current x-coordinate of the tile.
+     */
     getX() {
         return this.x;
     }
+
+    /**
+     * Set the x-coordinate of the tile.
+     * @param {number} newX - The new x-coordinate to set for the tile.
+     */
     setX(newX) {
         this.x = newX;
     }
 
+    /**
+     * Get the y-coordinate of the tile.
+     * @returns {number} The current y-coordinate of the tile.
+     */
     getY() {
         return this.y;
     }
 
+    /**
+     * Set the y-coordinate of the tile.
+     * @param {number} newY - The new y-coordinate to set for the tile.
+     */
     setY(newY) {
         this.y = newY;
     }
 
-    //Increments counter the first time a grid button is clicked
+    /*
+    *Increments the counter element the first time a grid button is clicked
+    */
     incrementClickCounter = () => {
         $("#counter").textContent = ('000' + (parseInt($("#counter").textContent) + 1)).slice(-3);
     }
 
-    // Reveals the value of the tile clicked, sets its color and increments the click counter
+    /*
+    * Reveals the value of the tile clicked, sets its color and increments the click counter
+    * Won't go through if the tile is marked as "isClicked"
+    */
     revealClickedTile = (evt) => {
         if (evt.target.isClicked == "false") {
             if (evt.target.actualValue != -1) {
@@ -53,7 +93,11 @@ class Tile {
         };
     };
 
-    // Triggers win when count of non-mine clicked tiles hits 0
+    /*
+    * Triggers win when count of non-mine clicked tiles hits 0
+    * Adds Win message to game area
+    * Disables the game board and stops the timer
+    */
     triggerWin = () => {
         if ($("#playArea").remainingTiles == 0) {
             console.log("You win!");
@@ -64,7 +108,10 @@ class Tile {
         };
     };
 
-    // Triggers a game loss and disables the board when a mine (-1) tile is clicked
+    /*
+    * Triggers a game loss, stops the timer and disables the board when a mine (-1) tile is clicked
+    * Adds loss message to game area
+    */
     triggerLoss = (evt) => {
         if (evt.target.actualValue == -1) {
             $("#gameResult").textContent = "You Lose!";
@@ -74,8 +121,10 @@ class Tile {
         }
     };
 
-    // Returns a color to be used for styling based on the tile value passed on
-    // colors codes are based on official minesweeper colors
+    /* Returns a color to be used for styling based on the tile value passed on
+    * colors codes are based on official minesweeper colors
+    * @returns {"string"} a string that represents a color than can be supplied to CSS styling
+    */
     determineTileColor = (tileValue) => {
         if (tileValue == "-1") {
             return "orange";
@@ -112,16 +161,19 @@ class Tile {
         }
     };
 
-    // Builds a button DOM element to represent a tile in the gameboard
+    /*
+    * Builds a button DOM element to represent a tile in the gameboard
+    */
     buildPageElement() {
         const currentButton = document.createElement("button");
         currentButton.id = this.x + "/" + this.y;
         currentButton.classList = "gridButton";
         currentButton.x = this.x;
         currentButton.y = this.y;
+        currentButton.isClicked = "false";
+
         // To cheat comment the line below and uncomment the line in board.js setting textContent to "this.boardGrid[currentButton.x][currentButton.y].getValue();"
         currentButton.textContent = "?";
-        currentButton.isClicked = "false";
 
         currentButton.addEventListener("click", this.revealClickedTile);
         currentButton.addEventListener("click", this.triggerLoss);
